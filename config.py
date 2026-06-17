876908b3f44323504e13eda045166d1789dd747d
Srinivasulu Padigay <125829496+keikazuki@users.noreply.github.com>
2026-06-17 18:43:48 -0500

    Refactor config.py to use placeholder values
    Updated configuration values to use placeholders for user credentials and API keys.

--- config.py
+++ config.py
@@ -1,29 +1,22 @@
 # -*- coding: utf-8 -*-
-
 """
 List of All Configurations
 """
 
-# Reddit configuration for DoujinStash
-USERNAME = "PERVTAKUS"
-PASSWORD = "mastervenki99"
-USERAGENT = "web:com.edu.lab:v1.5.8 (by /u/PERVTAKUS)"
-REDDITAPPID = "mTZx1ZZZQHRsgQ"
-REDDITAPPSECRET = "mfvGyhx8dZ-8eo1w5g1mz7ANsn8"
-
+# Reddit configuration
+USERNAME = "your_reddit_username"
+PASSWORD = "your_reddit_password"
+USERAGENT = "your_useragent"
+REDDITAPPID = "your_app_id"
+REDDITAPPSECRET = "your_app_secret"
 
 # Postgresql configuration
 DBNAME = "SauceMaster"
 DBUSER = "postgres"
 DBHOST = "127.0.0.1"
-DBPASSWORD = "shakimom"
+DBPASSWORD = "your_db_password"
 DBPORT = "5432"
 
 # Imgur configuration
-IMGUR_CLIENTID = "c5978a36b06f083"
-IMGUR_CLIENTSECRET = "cc47ca093ba9f2497f8a43798e3f14b7410b8fd3"
-
-
-def getSignature():
-    signature = "\n\n---\n\n^Tag the BOT to check reposts | Link {anime}, <manga>, ]LN[, |VN| | [Subreddit](https://www.reddit.com/r/SauceSharingCommunity/) | [FAQ](https://www.reddit.com/r/SauceSharingCommunity/wiki/index/saucesharingbot)"
-    return signature.replace(" ", "&#32;")
+IMGUR_CLIENTID = "your_imgur_client_id"
+IMGUR_CLIENTSECRET = "your_imgur_client_secret"
